8dbe2a9822d62633b900b7f7d63c3488c3bd36e8
reichardn <reichardn@gmail.com>
2016-08-17 16:30:14 -0400

    set html for users#index view

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,11 +1,47 @@
+class User {
+
+  constructor(id, email, diaries) {
+    this.id = id;
+    this.email = email;
+    this.diaries = diaries;
+  }
+  printName() {
+    return "User " + this.id + ": " + this.email;
+  }
+
+  diariesLength() {
+    return this.diaries.length
+  }
+}
+
+User.fromJSON = function (data) {
+  var user = new User(data['id'], data['email'], data['diaries']);
+  return user;
+}
+
+var bindShowUsersButton = function() {
+  $(".users-list").on("click", function(e) {
+    var id = parseInt($(".js-next").attr("data-id"));
+    $(".diaries").empty();
+    $.get("/users/" + id + ".json", function(data) {
+      var diaries = data['diaries'];
+      $.each(diaries, function(i, v) {
+        var html = "<li><a href='/diaries/" + v['id'] +"'>" +  v['created_at'] + "</a></li>";
+        $('.diaries').append(html);
+      })
+    });
+    e.preventDefault();
+  });
+};
+
 var bindNextButton = function() {
   $(".js-next").on("click", function(e) {
     var nextId = parseInt($(".js-next").attr("data-id")) + 1;
     $.get("/users/" + nextId + ".json", function(data) {
-      var name = "User" + data['id'] + ": " + data['email'];
-      $(".userEmail").text(name);
+      var user = User.fromJSON(data);
+      $(".userEmail").text(user.printName());
       $(".diaries").empty();
-      $(".view-diaries").text("View All " + data['diaries'].length + " Diaries");
+      $(".view-diaries").text("View All " + user.diariesLength() + " Diaries");
       // re-set the id to current on the link
       $(".js-next").attr("data-id", data["id"]);
     });
@@ -26,10 +62,10 @@ var bindShowDiaryButton = function() {
     });
     e.preventDefault();
   });
-
 };
 
 var bindUserClickEvents = function() {
   bindNextButton();
   bindShowDiaryButton();
 };
+
